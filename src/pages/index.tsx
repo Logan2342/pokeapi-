62e0cd8476477d827e6c1a6ea847a1710093805d
Logan2342 <bsftio28@gmail.com>
2024-07-09 14:20:20 -0700

    Update UI styling in index.tsx
    The UI styling of the main search elements in the landing page has been updated. Changes include cosmetic adjustments of border colors and positioning of elements. Also, the style of the search button has also been improved for better visibility.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
     const [searchQuery, setSearchQuery] = useState('');
@@ -8,23 +8,27 @@ export default function Home() {
         console.log('Search query:', searchQuery);
         // Implement your search logic here
     };
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center space-x-4">
-        <input
-            type="text"
-            className= "px-4 py-2 border rounded-lg focus:outline-none focus:ring-2-brown-500"
-            placeholder="Look up"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          <button
-              className="px-4 py-2 text-gray-500 border-gray-100 rounded-lg hover: bg-amber-50 focus:outline-none focus:ring-2 focus:ring-emerald-100"
-              onClick={handleSearch}
-              >
-              search
-          </button>
-      </div>
-    </main>
-  );
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="flex flex-row-reverse items-center space-x-reverse space-x-4 border-2 border-neutral-700 p-4">
+                <input
+                    type="text"
+                    className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-brown-500"
+                    placeholder="Look up"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button
+                    className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-emerald-500"
+                    onClick={handleSearch}
+                >
+                    Search
+                </button>
+            </div>
+            <div className="flex flex-row justify-between w-full border-2 border-blue-500 gap-4">
+
+            </div>
+        </main>
+    );
 }
